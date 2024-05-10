1d6013f76cb63783bfb174c12f4ab2f5d7b04f20
Sudarsun Santhiappan <sudar@buddi.ai>
2024-05-10 18:35:43 +0530

    added init script and updated the docker compose settings

---
+++ dataset/init.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE if not exists mta_data;
+CREATE TABLE if not exists mta_data.public.mta_reports(
+timestamp varchar NULL,
+trip_id varchar NULL,
+route_id varchar NULL,
+trip_start_time varchar NULL,
+trip_start_date varchar NULL,
+vehicle_id varchar NULL,
+vehicle_label varchar NULL,
+vehicle_license_plate varchar NULL,
+latitude varchar NULL,
+longitude varchar NULL,
+bearing varchar NULL,
+speed varchar NULL,
+stop_id varchar NULL,
+stop_status varchar NULL,
+occupancy_status varchar NULL,
+congestion_level varchar NULL,
+progress varchar NULL,
+block_assigned varchar NULL,
+dist_along_route varchar NULL,
+dist_from_stop varchar NULL);
+
